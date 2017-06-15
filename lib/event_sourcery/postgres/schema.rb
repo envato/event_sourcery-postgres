9aa47e4c08d748e10ef65e46a2bdda94a5c732e7
Orien Madgwick <_@orien.io>
2017-06-16 09:36:46 +1000

    Event store writes causation_id

--- lib/event_sourcery/postgres/schema.rb
+++ lib/event_sourcery/postgres/schema.rb
@@ -44,7 +44,7 @@ module EventSourcery
                                      events_table_name: EventSourcery::Postgres.config.events_table_name,
                                      aggregates_table_name: EventSourcery::Postgres.config.aggregates_table_name)
         db.run <<-SQL
-create or replace function #{function_name}(_aggregateId uuid, _eventTypes varchar[], _expectedVersion int, _bodies json[], _createdAtTimes timestamp without time zone[], _eventUUIDs uuid[], _lockTable boolean) returns void as $$
+create or replace function #{function_name}(_aggregateId uuid, _eventTypes varchar[], _expectedVersion int, _bodies json[], _createdAtTimes timestamp without time zone[], _eventUUIDs uuid[], _causationIds uuid[], _lockTable boolean) returns void as $$
 declare
 currentVersion int;
 body json;
@@ -104,9 +104,9 @@ loop
   end if;
 
   insert into #{events_table_name}
-    (uuid, aggregate_id, type, body, version, created_at)
+    (uuid, aggregate_id, type, body, version, causation_id, created_at)
   values
-    (_eventUUIDs[index], _aggregateId, _eventTypes[index], body, eventVersion, createdAt)
+    (_eventUUIDs[index], _aggregateId, _eventTypes[index], body, eventVersion, _causationIds[index], createdAt)
   returning id into eventId;
 
   eventVersion := eventVersion + 1;
